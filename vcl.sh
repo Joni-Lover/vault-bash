a550dfcc912855ac80ee338771405fd18257a555
Ivan Palanevich <ivan_palanevich@epam.com>
2017-05-25 12:58:22 +0300

    Add more functions

--- vcl.sh
+++ vcl.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+set -e
 
-vault_path=${1:-'secret/'}
+PARAM="${1}"
+vault_path=${2:-'secret/'}
+
+if [ $# -ne 1 ]
+then
+  echo "Usage:"
+  echo "$0 get-all"
+  echo "$0 get-all secret/bar/"
+  echo "$0 edit secret/foo"
+  echo "$0 push secret/foo"
+  exit 1
+fi
+
+lst_bin_to_check=(mkdir vault jq vim)
+
+function checkb {
+  local bin=$1
+  [ -f "$(which ${bin})" ] >/dev/null 2>&1 || \
+    { echo "Please install ${bin}"; exit $NOT_OK ;};
+}
+
+for inp in "${lst_bin_to_check[@]}";do
+  checkb "${inp}"
+done
 
 function getAll {
-  path="${1}"
+  local path="${1}"
   for i in `vault list "${path}" | grep -Ev 'Ke|-|^$'`;
   do
     if [ "${i: -1}" == '/' ]; then
@@ -17,4 +41,38 @@ function getAll {
   done
 }
 
-getAll "${vault_path}"
+function edit {
+  local path="${1}"
+  if [ "${path: -1}" == '/' ]; then
+    echo "Error, pass vaild secret path: ${path}"
+  else
+    vim ${path}
+  fi
+}
+
+function push {
+  local path="${1}"
+  if [ "${path: -1}" == '/' ]; then
+    echo "Error, pass vaild secret path: ${path}"
+  else
+    vault write ${path} @"${path}".json
+  fi
+}
+
+case "$PARAM" in
+  get-all )
+    getAll "${vault_path}"
+    ;;
+  edit )
+    edit "${vault_path}"
+    ;;
+  push )
+    push "${vault_path}"
+    ;;
+  *)
+    echo "Unknown argument: $PARAM "
+    exit 1
+    ;;
+esac
+
+exit 0
